59752b585f6a6136103a2beaeee466156a37a027
Vaibhav <vaibhavtiwary7766@gmail.com>
2025-07-08 10:18:33 +0530

    create header and footer component

--- ems-react/src/App.jsx
+++ ems-react/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
 import ListEmployee from './components/ListEmployee'
 
 function App() {
 
   return (
     <>
+      <Header />
       <ListEmployee />
+      <Footer />
     </>
   )
 }
